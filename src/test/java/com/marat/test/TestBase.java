dd00b00cb9cfa7b4514b260f7d8d7b3415a83787
MartinGPU <maratussr@gmail.com>
2023-02-28 21:04:35 +0400

    added string format

--- src/test/java/com/marat/test/TestBase.java
+++ src/test/java/com/marat/test/TestBase.java
@@ -2,13 +2,17 @@ package com.marat.test;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import com.marat.config.CredentialsConfig;
 import com.marat.helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
+
+    public static CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class);
 
     @BeforeAll
     public static void beforeAll() {
@@ -18,7 +22,10 @@ public class TestBase {
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+        String value1 = credentials.remoteLogin();
+        String value2 = credentials.remotePassword();
+        String value3 = credentials.remoteUrl();
+        Configuration.remote = String.format("https://%s:%s@%s", value1, value2, value3);
     }
 
     @AfterEach
